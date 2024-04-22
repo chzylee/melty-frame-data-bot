2183a6bc7350e512d5a67354bbaa3cb722fcf8fd
NOAH LEE <iamnoah@myself.com>
2024-04-22 12:38:26 -0500

    update command updater

--- command-management/update_commands.py
+++ command-management/update_commands.py
@@ -2,10 +2,14 @@ import requests
 import yaml
 import local_env
 
+SERVER_ID = local_env.SERVER_ID
 TOKEN = local_env.BOT_TOKEN
 APPLICATION_ID = "1231490345565683814"
-URL = f"https://discord.com/api/v9/applications/{APPLICATION_ID}/commands"
 
+URL = f"https://discord.com/api/v10/applications/{APPLICATION_ID}/guilds/{SERVER_ID}/commands"
+
+# global commands are cached and only update every hour
+# URL = f'https://discord.com/api/v10/applications/{APPLICATION_ID}/commands'
 
 with open("bot_commands.yaml", "r") as file:
     yaml_content = file.read()
@@ -18,3 +22,5 @@ for command in commands:
     response = requests.post(URL, json=command, headers=headers)
     command_name = command["name"]
     print(f"Command {command_name} created: {response.status_code}")
+
+print(response.json())
