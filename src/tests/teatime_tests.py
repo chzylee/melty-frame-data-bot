828549f83ee71e2e25f3288c82ca8cd9e7075019
NOAH LEE <iamnoah@myself.com>
2024-04-22 21:19:21 -0500

    reorganize and add tests

---
+++ src/tests/teatime_tests.py
@@ -0,0 +1,10 @@
+import unittest
+from app.commands import teatime
+
+class TestTeatime(unittest.TestCase):
+    def test_have_teatime_does_return_message(self):
+        self.assertIsNotNone(teatime.have_teatime())
+
+
+if __name__ == '__main__':
+    unittest.main()
