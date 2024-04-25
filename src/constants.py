c5310703f257684d1296ac0c78c20837f7eeb9ef
NOAH LEE <iamnoah@myself.com>
2024-04-25 17:17:20 -0500

    remove trailing space

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,10 @@
 # found on Discord Application -> General Information page
 PUBLIC_KEY = "652b00ff77b759a40d67c80271a60ff8d919356dfca21a1a86c7a8f83e2ea6b8"
 PING_PONG = { "type": 1 }
-RESPONSE_TYPES =  { 
-    "PONG": 1, 
-    "ACK_NO_SOURCE": 2, 
-    "MESSAGE_NO_SOURCE": 3, 
-    "MESSAGE_WITH_SOURCE": 4, 
+RESPONSE_TYPES =  {
+    "PONG": 1,
+    "ACK_NO_SOURCE": 2,
+    "MESSAGE_NO_SOURCE": 3,
+    "MESSAGE_WITH_SOURCE": 4,
     "ACK_WITH_SOURCE": 5
 }
