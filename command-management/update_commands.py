429f5ce7f3d09cedc4a87e22b18df194525cfd92
NOAH LEE <iamnoah@myself.com>
2024-04-24 22:06:33 -0500

    parametrize command update script for envs

--- command-management/update_commands.py
+++ command-management/update_commands.py
@@ -1,25 +1,36 @@
+import sys
 import requests
 import yaml
-import local_env
+from local_env import env_vars
 
-SERVER_ID = local_env.SERVER_ID
-TOKEN = local_env.BOT_TOKEN
-APPLICATION_ID = "1231490345565683814"
 
-# URL = f"https://discord.com/api/v10/applications/{APPLICATION_ID}/guilds/{SERVER_ID}/commands"
+env = input("Enter bot environment: ")
+
+if env.lower() != 'dev' and env.lower() != 'prod':
+    print("Allowed environments are 'dev' or 'prod'")
+    sys.exit()
+
+bot_token = env_vars[env]["bot_token"]
+application_id = env_vars[env]["application_id"]
+
+if env.lower() == 'dev':
+    server_id = env_vars[env]["server_id"]
+    url = f"https://discord.com/api/v10/applications/{application_id}/guilds/{server_id}/commands"
+else:
+    url = f'https://discord.com/api/v10/applications/{application_id}/commands'
 
 # global commands are cached and only update every hour
-URL = f'https://discord.com/api/v10/applications/{APPLICATION_ID}/commands'
+# URL = f'https://discord.com/api/v10/applications/{APPLICATION_ID}/commands'
 
 with open("bot_commands.yaml", "r") as file:
     yaml_content = file.read()
 
 commands = yaml.safe_load(yaml_content)
-headers = {"Authorization": f"Bot {TOKEN}", "Content-Type": "application/json"}
+headers = {"Authorization": f"Bot {bot_token}", "Content-Type": "application/json"}
 
 # Send the POST request for each command
 for command in commands:
-    response = requests.post(URL, json=command, headers=headers)
+    response = requests.post(url, json=command, headers=headers)
     command_name = command["name"]
     print(f"Command {command_name} created: {response.status_code}")
 
