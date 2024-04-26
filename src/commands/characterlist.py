250af995c56440ac1d1fec6964e5512cdd317737
NOAH LEE <iamnoah@myself.com>
2024-04-25 23:10:54 -0500

    fix type

--- src/commands/characterlist.py
+++ src/commands/characterlist.py
@@ -1,7 +1,7 @@
 from typing import List
 from data import mizuumi
 
-def get_allowed_names() -> List[str]:
+def get_allowed_names() -> str:
     char_names = [name.capitalize() for name in mizuumi.CHAR_MAPPINGS.keys()]
     joined_names = ", ".join(char_names)
     return f"Allowed names for `/framedata` command:\n```{joined_names}```"
