075d8532fb614d2740e255a8b48037c5060077d0
NOAH LEE <iamnoah@myself.com>
2024-04-26 01:26:23 -0500

    add input matching

---
+++ src/data/inputs.py
@@ -0,0 +1,40 @@
+import re
+
+def _match_ground_move(input: str, regex: re.Pattern) -> str:
+    match = input.upper() if re.match(pattern=regex, string=input) else None
+    return match
+
+def _match_air_move(input: str, regex: re.Pattern) -> str:
+    match = None
+    if re.match(pattern=regex, string=input):
+        match_end = input.upper()[1:]
+        # Ensure we add "." after j if it was not included in input.
+        match = f"j.{match_end}" if input[1] != "." else f"j{match_end}"
+    return match
+
+def match_ground_normal(input: str) -> str:
+    # Allow lowercase in input and allow charged input (e.g. 5[C])
+    ground_normal_regex = r"[1-9](([AaBbCc])|(\[[AaBbCc]\]))"
+    # match() returns None on failure to match which fails if cond.
+    match = _match_ground_move(input, ground_normal_regex)
+    return match
+
+def match_air_normal(input: str) -> str:
+    air_normal_regex = r"[Jj]\.?(([AaBbCc])|(\[[AaBbCc]\]))" # Start with J since we made input upper.
+    match = _match_air_move(input, air_normal_regex)
+    return match
+
+def match_ground_special(input: str) -> str:
+    ground_special_regex = r"[1-6]{2,3}(([AaBbCc])|(\[[AaBbCc]\]))"
+    match = _match_ground_move(input, ground_special_regex)
+    return match
+
+def match_air_cmd_normal(input: str) -> str:
+    air_cmd_normal_regex = r"[Jj]\.?[1-9](([AaBbCc])|(\[[AaBbCc]\]))" # Start with J since we made input upper.
+    match = _match_air_move(input, air_cmd_normal_regex)
+    return match
+
+def match_air_special(input: str) -> str:
+    air_special_regex = r"[Jj]\.?[1-6]{2,3}(([AaBbCc])|(\[[AaBbCc]\]))" # Start with J since we made input upper.
+    match = _match_air_move(input, air_special_regex)
+    return match
---
+++ src/tests/inputs_tests.py
@@ -0,0 +1,79 @@
+import unittest
+from data import inputs
+
+class TestInputs(unittest.TestCase):
+    def test_match_ground_normal_given_valid_input_returns_formatted_input(self):
+        input = inputs.match_ground_normal("5b")
+        self.assertEqual(input, "5B") # Button should be capitalized.
+
+    def test_match_ground_normal_given_charged_input_returns_formatted_input(self):
+        input = inputs.match_ground_normal("5[c]")
+        self.assertEqual(input, "5[C]")
+
+    def test_match_ground_normal_given_invalid_input_returns_None(self):
+        input = inputs.match_ground_normal("B") # Not a valid normal.
+        self.assertIsNone(input)
+
+    def test_match_air_normal_given_valid_input_returns_formatted_input(self):
+        input = inputs.match_air_normal("j.c")
+        self.assertEqual(input, "j.C")
+
+    def test_match_air_normal_given_charged_input_returns_formatted_input(self):
+        input = inputs.match_air_normal("j.[c]")
+        self.assertEqual(input, "j.[C]")
+
+    def test_match_air_normal_given_no_period_after_j_returns_formatted_input(self):
+        input = inputs.match_air_normal("jC")
+        self.assertEqual(input, "j.C") # Add "." even when not given.
+
+    def test_match_air_normal_given_invalid_input_returns_None(self):
+        input = inputs.match_air_normal("j5A") # Not valid air normal notation.
+        self.assertIsNone(input)
+
+    def test_match_ground_special_given_valid_input_returns_formatted_input(self):
+        input = inputs.match_ground_special("623a")
+        self.assertEqual(input, "623A")
+
+    def test_match_ground_special_given_charged_input_returns_formatted_input(self):
+        input = inputs.match_ground_special("623[b]")
+        self.assertEqual(input, "623[B]")
+
+    def test_match_ground_special_given_22_input_returns_formatted_input(self):
+        input = inputs.match_ground_special("22b")
+        self.assertEqual(input, "22B")
+
+    def test_match_ground_normal_given_invalid_input_returns_None(self):
+        input = inputs.match_ground_special("63214C") # Not a valid special.
+        self.assertIsNone(input)
+
+    def test_match_air_cmd_normal_given_valid_input_returns_formatted_input(self):
+        input = inputs.match_air_cmd_normal("j.6a")
+        self.assertEqual(input, "j.6A")
+
+    def test_match_air_cmd_normal_given_charged_input_returns_formatted_input(self):
+        input = inputs.match_air_cmd_normal("j.2[c]")
+        self.assertEqual(input, "j.2[C]")
+
+    def test_match_air_cmd_normal_given_no_period_after_j_returns_formatted_input(self):
+        input = inputs.match_air_cmd_normal("j6a")
+        self.assertEqual(input, "j.6A")
+
+    def test_match_air_cmd_normal_given_invalid_input_returns_None(self):
+        input = inputs.match_air_cmd_normal("jC") # Not valid air cmd normal notation.
+        self.assertIsNone(input)
+
+    def test_match_air_special_given_valid_input_returns_formatted_input(self):
+        input = inputs.match_air_special("j.236b")
+        self.assertEqual(input, "j.236B")
+
+    def test_match_air_special_given_charged_input_returns_formatted_input(self):
+        input = inputs.match_air_special("j214[a]")
+        self.assertEqual(input, "j.214[A]")
+
+    def test_match_air_special_given_no_period_after_j_returns_formatted_input(self):
+        input = inputs.match_air_special("j236b")
+        self.assertEqual(input, "j.236B")
+
+    def test_match_air_special_given_invalid_input_returns_None(self):
+        input = inputs.match_air_special("j2B") # Not valid air special notation.
+        self.assertIsNone(input)
