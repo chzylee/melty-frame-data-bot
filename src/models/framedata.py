0fb85fdd1d09629323027b713146294286f96835
NOAH LEE <iamnoah@myself.com>
2024-04-28 13:20:15 -0500

    add invuln to model

--- src/models/framedata.py
+++ src/models/framedata.py
@@ -4,11 +4,21 @@ class MoveFrameData:
     recovery: str
     frame_adv: str
     proration: str
+    invuln: str
 
     # All properties required.
-    def __init__(self, first_active, active, recovery, frame_adv, proration):
+    def __init__(
+        self,
+        first_active: str,
+        active: str,
+        recovery: str,
+        frame_adv: str,
+        proration: str,
+        invuln: str
+    ):
         self.first_active = first_active
         self.active = active
         self.recovery = recovery
         self.frame_adv = frame_adv
         self.proration = proration
+        self.invuln = invuln
